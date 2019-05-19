6ead7c1fdb78a7f1e170af413904e05afa212436
cybor97 <muhametshin.1997@gmail.com>
2019-05-19 22:07:17 +0300

    Fixed bug with opposite transactions

--- routes/clientApi.js
+++ routes/clientApi.js
@@ -88,7 +88,7 @@ router
             raw: true
         }))[0];
 
-        let whereParams = {from: req.params.address};
+        let whereParams = {[sequelize.Op.or]: [{from: req.params.address}, {to: req.params.address}]};
         if(req.query.currency){
             whereParams.currency = req.query.currency;
         }
