7dce21c465fe95ad701258520b2eab8675eb945e
cybor97 <muhametshin.1997@gmail.com>
2019-05-19 21:23:20 +0300

    Fixed ORBS data collection

--- connectors/orbs.js
+++ connectors/orbs.js
@@ -3,6 +3,8 @@ const BaseConnector = require('./baseConnector');
 
 const CONTRACT_HASH = '0x30f855afb78758Aa4C2dc706fb0fA3A98c865d2d';
 const TOPIC_DELEGATE = '0x510b11bb3f3c799b11307c01ab7db0d335683ef5b2da98f7697de744f465eacc';
+const PRECENDING_ZEROES = '0'.repeat(24);
+const VALUE_FEE_MULTIPLIER = Math.pow(10, 18);
 
 class TEZ extends BaseConnector {
     constructor(){
@@ -13,9 +15,9 @@ class TEZ extends BaseConnector {
     async getAllTransactions(address){
         //ETH has a bit longer addresses with precending 0-es
         if(address.length === 42){
-            address = '0xb1aa947c0f11ac2b732167ea53dfbb5f4ab7da88'.replace('0x', `0x${'0'.repeat(24)}`);
+            address = address.replace('0x', `0x${PRECENDING_ZEROES}`);
         }
-
+        //FIXME: Consider re-implement with RPCs eth_getLogs&eth_getTransactionByHash
         return (await axios.get(this.apiUrl, {
             params: {
                 module: 'logs',
@@ -32,15 +34,20 @@ class TEZ extends BaseConnector {
             .map(tx => {
                 return ({
                     //0 is methodId
-                    from: tx.topics[1],
-                    to: tx.topics[2],
+                    from: tx.topics[1].replace(`0x${PRECENDING_ZEROES}`, '0x'),
+                    to: tx.topics[2].replace(`0x${PRECENDING_ZEROES}`, '0x'),
                     hash: tx.transactionHash,
-                    date: new Date(parseInt(tx.timeStamp) * 1000).getTime(),
+                    date: parseInt(tx.timeStamp) * 1000,
                     //always 0 for delegation
                     value: 0,
                     fromAlias: null,
-                    fee: parseInt(tx.gasUsed) * parseInt(tx.gasPrice)/1000000000/1000000000,
-                    type: 'delegation'
+                    fee: parseInt(tx.gasUsed) * parseInt(tx.gasPrice)/VALUE_FEE_MULTIPLIER,
+                    type: 'delegation',
+                    path: JSON.stringify({
+                        blockNumber: parseInt(tx.blockNumber), 
+                        transactionIndex: parseInt(tx.transactionIndex)
+                    }),
+                    originalOpType: 'transaction'
                 })
             });
     }
