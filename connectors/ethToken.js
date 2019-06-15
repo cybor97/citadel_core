b49e3eff37823f941b0c6c987661c60f9e706692
cybor97 <muhametshin.1997@gmail.com>
2019-06-15 22:28:40 +0300

    Fixed issue with direct node calls from frontend

--- connectors/ethToken.js
+++ connectors/ethToken.js
@@ -53,30 +53,51 @@ class ETHToken extends BaseConnector {
         let contractAddress = this.getTransferContractAddress();
         let contract = new web3.eth.Contract(this.getTransferABI(), contractAddress);
         let transferData = contract.methods.transfer(toAddress, web3.utils.toHex(amount * VALUE_FEE_MULTIPLIER));
+        let transactionCount = await web3.eth.getTransactionCount(fromAddress, 'latest');
+        let gasPrice = await web3.eth.getGasPrice();
+        let chainId = await web3.eth.getChainId();
+
         const abi = transferData.encodeABI();
 
-        let transfer = {to: contractAddress, data: abi, gas: 200000/**Recommended for tokens */};
+        let transfer = {
+            to: contractAddress, 
+            data: abi, 
+            gas: 200000,/**Recommended for tokens */
+            nonce: transactionCount,
+            gasPrice: gasPrice,
+            chainId: chainId
+        };
         return transfer;
     }
 
-    prepareDelegation(fromAddress, toAddress){
+    async prepareDelegation(fromAddress, toAddress){
         let web3 = new Web3(`http://${config.parity.ip}:${config.parity.port}`);
         let contractAddress = this.getDelegationContractAddress();
         let contract = new web3.eth.Contract(this.getDelegateABI(), contractAddress);
         let delegateData = contract.methods.delegate(toAddress).encodeABI();
-        let delegation = {to: contractAddress, data: delegateData};
+        let transactionCount = await web3.eth.getTransactionCount(fromAddress, 'latest');
+        let gasPrice = await web3.eth.getGasPrice();
+        let chainId = await web3.eth.getChainId();
+
+        let delegation = {
+            to: contractAddress, 
+            data: delegateData, 
+            gas: 200000,
+            nonce: transactionCount,
+            gasPrice: gasPrice,
+            chainId: chainId            
+        };
         return delegation;
     }
 
     async sendTransaction(address, signedTransaction){
         let web3 = new Web3(`http://${config.parity.ip}:8545`);
         //TODO: Add address validation
-
-        return await new Promise((resolve, reject) => 
+        return new Promise((resolve, reject)=>{
             web3.eth.sendSignedTransaction(signedTransaction)
-                .on('receipt', resolve)
-                .on('error', reject)
-        );
+                .on('transactionHash', resolve)
+                .on('error', reject);
+        });
     }
 }
 
