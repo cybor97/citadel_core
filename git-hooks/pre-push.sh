81c5124f9466c60e6a9813259f44247ed77524f3
cybor97 <muhametshin.1997@gmail.com>
2019-07-11 21:16:57 +0300

    Fixed build number increment

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -2,7 +2,7 @@
 ./build.sh;
 BUILD_RESULT=$?;
 if [[ $BUILD_RESULT == 0 ]]; then
-    git tag build$(expr `git tag|grep "build[0-9]*"|grep "[0-9]*" -o` + 1);
+    git tag build$((`git tag|grep "build[0-9]*"|grep "[0-9]*" -o|tail -1` + 1));
     git push --tags;
 else
     git reset origin/$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
