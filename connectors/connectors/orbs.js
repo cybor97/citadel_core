c26e3bafdb27773d475b2e580388527d8d775168
cybor97 <muhametshin.1997@gmail.com>
2019-10-13 16:41:51 +0300

    Reduced query blocks count or orbs

--- connectors/connectors/orbs.js
+++ connectors/connectors/orbs.js
@@ -8,7 +8,7 @@ const config = require('../../config')
 const log = require('../../utils/log');
 
 const STARTS_FROM_BLOCK = 5710114;
-const BLOCKS_QUERY_COUNT = 500;
+const BLOCKS_QUERY_COUNT = 10;
 
 const DELEGATE_CONTRACT_HASH = '0x30f855afb78758Aa4C2dc706fb0fA3A98c865d2d';
 const DELEGATE_TOPIC = '0x510b11bb3f3c799b11307c01ab7db0d335683ef5b2da98f7697de744f465eacc';
