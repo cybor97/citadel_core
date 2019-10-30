4b4c7013fa07a39820b0780560c54cb92023bee7
cybor97 <muhametshin.1997@gmail.com>
2019-10-30 23:39:40 +0300

    Fixed iostCoinPayments migration for postgres

--- data/migrations/20191030000000-iostCoinPayments.js
+++ data/migrations/20191030000000-iostCoinPayments.js
@@ -5,8 +5,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return await queryInterface.sequelize.query(`
     UPDATE transactions 
-    SET \`type\` = 'payment'
-    WHERE \`type\` = 'supplement' AND currency = 'iost-coin' AND \`from\` IN ('bonus.iost', 'vote_producer.iost')`);
+    SET "type" = 'payment'
+    WHERE "type" = 'supplement' AND currency = 'iost-coin' AND "from" IN ('bonus.iost', 'vote_producer.iost')`);
   },
 
   down: (queryInterface, Sequelize) => {
