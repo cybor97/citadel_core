dc91b88747576c41f7f5fe3a797e26338c2813fd
cybor97 <muhametshin.1997@gmail.com>
2019-11-28 00:10:24 +0300

    Fixed build&temporary disabled icon

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,13 +9,15 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '2G',
-  }, ...Object.keys(connectors).map((connectorName, i) => ({
-    name: `#${i} Worker(${connectorName})`,
-    script: 'app.js',
-    args: ['--worker', `--net=${connectorName}`],
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '2G',
-  }))],
+  }, ...Object.keys(connectors)
+    .filter(connectorName => connectorName === 'icon')
+    .map((connectorName, i) => ({
+      name: `#${i} Worker(${connectorName})`,
+      script: 'app.js',
+      args: ['--worker', `--net=${connectorName}`],
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+    }))],
 };
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 unzip -o citadel_core.zip -d /srv/citadel_core;
 rm -f citadel_core.zip;
 cd /srv/citadel_core;
-rm -rf /node_modules;
+rm -rf node_modules;
 ./build.sh;
 pm2 restart all;
 rm -f install.sh;
