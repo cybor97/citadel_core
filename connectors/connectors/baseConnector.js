b71120c687874170deb10ae7f3c688d5188fde8f
cybor97 <muhametshin.1997@gmail.com>
2019-12-03 20:31:33 +0300

    Fixed bug with no "zabbix" in config

--- connectors/connectors/baseConnector.js
+++ connectors/connectors/baseConnector.js
@@ -1,20 +1,23 @@
-//TODO: Implement as singleton
 class BaseConnector {
     getAllTransactions() {
 
     }
 
     async sendZabbix(data) {
-        this.zabbixSender.clearItems();
-        Object.keys(data).forEach(key => this.zabbixSender.addItem(`connector.${key}`, data[key]));
-        await new Promise((resolve, reject) => this.zabbixSender.send((err, res) => {
-            if (err) {
-                return reject(err);
+        if (this.zabbixSender) {
+            if (this.zabbixSender.clearItems) {
+                this.zabbixSender.clearItems();
             }
-            else {
-                return resolve(res);
-            }
-        }));
+            Object.keys(data).forEach(key => this.zabbixSender.addItem(`connector.${key}`, data[key]));
+            await new Promise((resolve, reject) => this.zabbixSender.send((err, res) => {
+                if (err) {
+                    return reject(err);
+                }
+                else {
+                    return resolve(res);
+                }
+            }));
+        }
     }
 }
 
