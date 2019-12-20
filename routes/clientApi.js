5f2bc67b6228569ba536ab7de2d06720df0e91a1
cybor97 <muhametshin.1997@gmail.com>
2019-12-20 18:29:21 +0300

    Removed date_from & date_to for chart-below

--- routes/clientApi.js
+++ routes/clientApi.js
@@ -485,15 +485,12 @@ router
             return res.status(403).send({ message: 'Unauthorized!' });
         }
 
-        let now = Date.now();
-        let dateFrom = req.query.date_from || now - (4 * 7 * 24 * 3600 * 1000);
-        let dateTo = req.query.date_to || now;
         let stepOverride = req.query.step;
         if (stepOverride) {
             stepOverride *= 60 * 1000;
         }
 
-        let result = await explorerUpdater.getChartData(req.params.userId, req.params.net, req.query.address, dateFrom, dateTo, !req.query.add_balance, stepOverride || (7 * 24 * 3600 * 1000));
+        let result = await explorerUpdater.getChartData(req.params.userId, req.params.net, req.query.address, 0, Date.now(), !req.query.add_balance, stepOverride || (7 * 24 * 3600 * 1000));
 
         return res.status(200).send(result);
     })
