b00c4dd95c6e30e6a2300de5e15fda1221f6784d
Ruslan <muhametshin.1997@gmail.com>
2020-01-14 21:31:16 +0300

    Fixed deltaFromTo migration

--- data/migrations/20200114000000-deltaFromTo.js
+++ data/migrations/20200114000000-deltaFromTo.js
@@ -4,15 +4,15 @@ const config = require('../../config');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return await Promise.all([
-      queryInterface.sequelize.query('ALTER TABLE transactions ADD COLUMN `deltaFrom` FLOAT;'),
-      queryInterface.sequelize.query('ALTER TABLE transactions ADD COLUMN `deltaTo` FLOAT;')
+      queryInterface.sequelize.query('ALTER TABLE transactions ADD COLUMN "deltaFrom" FLOAT;'),
+      queryInterface.sequelize.query('ALTER TABLE transactions ADD COLUMN "deltaTo" FLOAT;')
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
     return await Promise.all([
-      queryInterface.sequelize.query('ALTER TABLE transactions DROP COLUMN `deltaFrom`;'),
-      queryInterface.sequelize.query('ALTER TABLE transactions DROP COLUMN `deltaTo`;')
+      queryInterface.sequelize.query('ALTER TABLE transactions DROP COLUMN "deltaFrom";'),
+      queryInterface.sequelize.query('ALTER TABLE transactions DROP COLUMN "deltaTo";')
     ]);
   }
 };
--- data/migrations/20200114000000-deltaFromToUpdate.js
+++ data/migrations/20200114000000-deltaFromToUpdate.js
@@ -3,7 +3,7 @@ const config = require('../../config');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return await queryInterface.sequelize.query('UPDATE transactions SET `deltaFrom` = -transactions.value, `deltaTo` = transactions.value;');
+    return await queryInterface.sequelize.query('UPDATE transactions SET "deltaFrom" = -transactions.value, "deltaTo" = transactions.value WHERE currency = \'tez\' AND "deltaFrom" IS NULL;');
   },
 
   down: async (queryInterface, Sequelize) => {
